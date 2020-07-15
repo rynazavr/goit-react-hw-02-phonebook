2fdc8fd999515f80b321cbc7fec4384a71a95222
rynazavr <lunevaira@gmail.com>
2020-07-15 22:37:20 +0300

    with clean fields

--- src/Components/Phonebook/Phonebook.js
+++ src/Components/Phonebook/Phonebook.js
@@ -5,15 +5,20 @@ import ContactList from "../ContactList/ContactList";
 import shortid from "shortid";
 
 class Phonebook extends Component {
+  formInitialState = {
+    name: "",
+    number: "",
+    filter: "",
+  };
   state = {
     name: "",
     number: "",
     filter: "",
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      { id: shortid.generate(), name: "Rosie Simpson", number: "459-12-56" },
+      { id: shortid.generate(), name: "Hermione Kline", number: "443-89-12" },
+      { id: shortid.generate(), name: "Eden Clements", number: "645-17-79" },
+      { id: shortid.generate(), name: "Annie Copeland", number: "227-91-26" },
     ],
   };
 
@@ -33,6 +38,7 @@ class Phonebook extends Component {
     this.setState((prevState) => ({
       contacts: [...prevState.contacts, singleContact],
     }));
+    this.setState({ ...this.formInitialState });
   };
 
   //   deleteContact = (id) => {
